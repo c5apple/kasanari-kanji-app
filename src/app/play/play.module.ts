23a92c3054081e5e33062e12c64b78a22d602383
c5apple <c5apple_party@yahoo.co.jp>
2019-01-09 00:29:41 +0900

    コンポーネント準備

---
+++ src/app/play/play.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PlayComponent } from './play.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [PlayComponent]
+})
+export class PlayModule { }
